9f4a568bb1dd8b110417b8abb1f6cae13d6be0ba
Olayinka-1224/Betty <lawalolayinka1224@gmail.com>
2022-12-08 16:11:34 -0800

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
  * main -Print a line of code using puts
  *
  * Return: 0
- *
- */
+*/
 
- int main(void)
- {
- 	 puts("\" Programming is like building a multilingual puzzle");
- 	 return (0);
- }
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
