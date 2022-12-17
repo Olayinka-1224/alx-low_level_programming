1539f6d88d76c247991680bd30d617c34de6f8f2
Olayinka-1224/alx-low_level_programming <lawalolayinka1224@gmail.com>
2022-12-17 15:30:19 -0800

    task 4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** main - prints all single number of base 10 starting from 0.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
