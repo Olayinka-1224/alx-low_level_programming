b219566e03da6949032b718ea9821d2180a9beab
Olayinka-1224/alx-low_level_programming <lawalolayinka1224@gmail.com>
2022-12-21 19:46:30 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - join two string with a bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
