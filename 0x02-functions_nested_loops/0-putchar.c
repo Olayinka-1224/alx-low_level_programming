fe8e3cc3274ec3896aca36a85a562a6a22bca7ef
Ahmed Lawal <lawalolayinka1224@gmail.com>
2023-01-07 22:43:40 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints putchar follow by a new line.
+ * main - prints _putchar follow by a new line.
  *
  * Return: 0
  */
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
